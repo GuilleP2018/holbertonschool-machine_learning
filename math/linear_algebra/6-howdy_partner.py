227b8de822e6f380290b64b452695c76dac66479
Guillep2018 <gpereyo@gmail.com>
2024-04-11 19:05:32 -0400

    6 complete - concat arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" Function that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """_summary_
+
+    Args:
+        arr1: a list of ints/floats
+        arr2: a list of ints/floats
+    Returns:
+        returns the new list of concatenated elements
+    """
+    concat_array = arr1 + arr2
+    return(concat_array)
