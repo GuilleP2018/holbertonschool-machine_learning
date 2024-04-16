d8b9072afab3e767adda67535f66ef560fa7db55
Guillep2018 <gpereyo@gmail.com>
2024-04-15 21:21:44 -0400

    4 completed

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
+"""Generate a plot of a histogram"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def frequency():
 
+def frequency():
+    """Generate a plot of a histogram"""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
+
+    # Plotting
+    bins = range(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+
+    # Labels and title
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+
+    plt.show()
