27498f2c6735d500deddc20488063f17d17c419e
Guillep2018 <gpereyo@gmail.com>
2024-04-16 17:08:41 -0400

    pycodestyle fix

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""Generate a bar graph with stacked bars."""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def bars():
+    """Generate a bar graph with stacked bars."""
     np.random.seed(5)
     fruit = np.random.randint(0, 20, (4,3))
     plt.figure(figsize=(6.4, 4.8))
