138cefdb4f51e2a90fb391a2ae5282691f5f25f4
Guillep2018 <gpereyo@gmail.com>
2024-04-15 20:13:41 -0400

    updates

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+"""Line plot"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 def line():
-
+    """Line plot"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
+
+    x = np.arange(0, 11)
+    plt.plot(x, y, 'r-')  # 'r-' specifies a red line
+    plt.xlim(0, 10)  # Set the x-axis range from 0 to 10
+    plt.show()
+
