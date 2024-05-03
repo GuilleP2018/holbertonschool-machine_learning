620eb3cac3fdef47f1026e7aa12f296be2f8914f
Guillep2018 <gpereyo@gmail.com>
2024-05-03 18:39:17 -0400

    0 complete-> neuron class created

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This module creates a neuron class"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron class"""
+    def __init__(self, nx):
+        """Function that initializes the data in the class"""
+        if isinstance(nx, int) is False:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.randn(nx).reshape(1, nx)
+        self.b = 0
+        self.A = 0
