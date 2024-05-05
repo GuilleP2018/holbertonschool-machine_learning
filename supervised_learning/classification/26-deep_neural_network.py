099433ee455ab78a593ab7cf6c65f1ac9b1c8cef
Guillep2018 <gpereyo@gmail.com>
2024-05-04 20:46:31 -0400

    26 pycode fix

--- supervised_learning/classification/26-deep_neural_network.py
+++ supervised_learning/classification/26-deep_neural_network.py
@@ -88,7 +88,8 @@ class DeepNeuralNetwork:
             self.__weights["b" + str(i)] = \
                 self.weights["b" + str(i)] - alpha * db
 
-    def train(self, X, Y, iterations=5000, alpha=0.05, verbose=True, graph=True, step=100):
+    def train(self, X, Y, iterations=5000, alpha=0.05,
+              verbose=True, graph=True, step=100):
         """Function that trains the deep neural network"""
         if not isinstance(iterations, int):
             raise TypeError("iterations must be an integer")
