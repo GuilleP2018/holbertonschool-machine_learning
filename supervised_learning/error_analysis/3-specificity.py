eaf085ce097d99b11f0f705870f1cba58ff75f29
Guillep2018 <gpereyo@gmail.com>
2024-05-23 17:37:51 -0400

    3 fixed - pycodestyle error

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -7,7 +7,8 @@ import numpy as np
 def specificity(confusion):
     """Calculate the specificity for each class in a confusion matrix"""
 
-    TN = np.sum(confusion) - (np.sum(confusion, axis=1) + np.sum(confusion, axis=0) - np.diag(confusion))
+    TN = np.sum(confusion) - (np.sum(confusion, axis=1) +
+                              np.sum(confusion, axis=0) - np.diag(confusion))
     FP = np.sum(confusion, axis=0) - np.diag(confusion)
 
     return TN / (TN + FP)
