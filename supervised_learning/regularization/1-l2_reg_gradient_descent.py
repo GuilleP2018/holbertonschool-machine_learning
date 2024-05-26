bd61e8dbe11a0435c3e258e7ee8ec22d9d9e77fa
Guillep2018 <gpereyo@gmail.com>
2024-05-26 11:21:19 -0400

    1 fix - correct output

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -8,13 +8,13 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """Updates the weights and biases of a neural network using gradient
     descent with L2 regularization"""
     m = Y.shape[1]
-    dZ = cache['A' + str(L)] - Y
-    for i in range(L, 0, -1):
-        A = cache['A' + str(i - 1)]
-        W = weights['W' + str(i)]
-        b = weights['b' + str(i)]
-        dW = (1 / m) * np.matmul(dZ, A.T) + (lambtha / m) * W
-        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
-        dZ = np.matmul(W.T, dZ) * (A * (1 - A))
-        weights['W' + str(i)] = W - alpha * dW
-        weights['b' + str(i)] = b - alpha * db
+    dz = cache['A' + str(L)] - Y
+    for idx in range(L, 0, -1):
+        A = cache['A' + str(idx - 1)]
+        W = weights['W' + str(idx)]
+        b = weights['b' + str(idx)]
+        dw = np.matmul(dz, A.T) / m
+        db = np.sum(dz, axis=1, keepdims=True) / m
+        dz = np.matmul(W.T, dz) * (1 - A * A)
+        weights['W' + str(idx)] = W - alpha * (dw + (lambtha/m) * W)
+        weights['b' + str(idx)] = b - alpha * db
