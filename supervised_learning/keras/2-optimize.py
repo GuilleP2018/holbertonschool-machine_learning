a91baedea8222337c1448d741c429588dbe1bdb9
Guillep2018 <gpereyo@gmail.com>
2024-06-01 15:05:48 -0400

    2 and 3 completed

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Optimize Module """
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    Sets up Adam optimization for a Keras
+    model with categorical crossentropy loss and accuracy metrics.
+    """
+    # Set up the Adam optimizer with the provided parameters
+    optimizer = K.optimizers.Adam(
+        learning_rate=alpha, beta_1=beta1, beta_2=beta2)
+
+    # Compile the model with the optimizer, loss function, and metrics
+    network.compile(optimizer=optimizer, loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+
+    return None
---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" One Hot Module """
+
+
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """
+    Converts a label vector into a one-hot matrix.
+    """
+    one_hot_matrix = K.utils.to_categorical(labels, num_classes=classes)
+    return one_hot_matrix
