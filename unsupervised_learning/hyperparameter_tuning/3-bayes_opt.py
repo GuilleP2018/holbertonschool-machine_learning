b6f5c2c5d5936b82e8e54e134ecf6131bdd643cf
Guillep2018 <gpereyo@gmail.com>
2024-08-17 16:40:50 -0400

    3

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -10,7 +10,7 @@ class BayesianOptimization:
     def __init__(self, f, X_init, Y_init, bounds,
                  ac_samples, l=1, sigma_f=1, xsi=0.01,
                  minimize=True):
-        """Init function for BayesianOptimization"""
+        """Init function for BayesianOptimization class"""
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
         self.X_s = np.linspace(
