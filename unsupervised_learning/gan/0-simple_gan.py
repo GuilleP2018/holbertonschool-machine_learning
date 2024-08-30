011d2d20c6010a98f72e5edd746b04e33bebe21f
Guillep2018 <gpereyo@gmail.com>
2024-08-30 14:45:39 -0400

    pycodestyle fix

--- unsupervised_learning/gan/0-simple_gan.py
+++ unsupervised_learning/gan/0-simple_gan.py
@@ -5,12 +5,14 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Simple_GAN(keras.Model) :
+
+class Simple_GAN(keras.Model):
     """Simple GAN class that inherits from keras.Model"""
-    def __init__( self, generator , discriminator , latent_generator, real_examples,
-                  batch_size=200, disc_iter=2, learning_rate=.005):
+    def __init__(self, generator, discriminator, latent_generator,
+                 real_examples,
+                 batch_size=200, disc_iter=2, learning_rate=.005):
         """Init function for the Simple_GAN class"""
-        super().__init__() # run the __init__ of keras.Model first.
+        super().__init__()  # run the __init__ of keras.Model first.
         self.latent_generator = latent_generator
         self.real_examples = real_examples
         self.generator = generator
@@ -18,11 +20,11 @@ class Simple_GAN(keras.Model) :
         self.batch_size = batch_size
         self.disc_iter = disc_iter
         self.learning_rate = learning_rate
-        self.beta_1=.5  # standard value, but can be changed if necessary
-        self.beta_2=.9  # standard value, but can be changed if necessary
+        self.beta_1 = .5  # standard value, but can be changed if necessary
+        self.beta_2 = .9  # standard value, but can be changed if necessary
 
         # define the generator loss and optimizer:
-        self.generator.loss = lambda x : tf.keras.losses.MeanSquaredError()(
+        self.generator.loss = lambda x: tf.keras.losses.MeanSquaredError()(
             x, tf.ones(x.shape))
 
         self.generator.optimizer = keras.optimizers.Adam(
@@ -31,27 +33,32 @@ class Simple_GAN(keras.Model) :
             beta_2=self.beta_2)
 
         self.generator.compile(optimizer=generator.optimizer,
-                                loss=generator.loss )
+                               loss=generator.loss)
 
-        # define the discriminator loss and optimizer:
-        self.discriminator.loss = lambda x,y : tf.keras.losses.MeanSquaredError()(
-            x, tf.ones(x.shape)) + tf.keras.losses.MeanSquaredError()(
-                y, -1*tf.ones(y.shape))
+        mse = tf.keras.losses.MeanSquaredError
+        ons = tf.ones
+
+        self.discriminator.loss = (
+            lambda x, y: (
+                mse()(x, ons(x.shape)) +
+                mse()(y, -1 * ons(y.shape))
+            )
+        )
 
         self.discriminator.optimizer = keras.optimizers.Adam(
             learning_rate=self.learning_rate,
             beta_1=self.beta_1,
             beta_2=self.beta_2)
 
-        self.discriminator.compile(optimizer=discriminator.optimizer ,
-                                   loss=discriminator.loss )
+        self.discriminator.compile(optimizer=discriminator.optimizer,
+                                   loss=discriminator.loss)
 
     # generator of real samples of size batch_size
     def get_fake_sample(self, size=None, training=False):
         """Get a fake sample of size batch_size"""
         # Verification of size and asigning value to size
-        if not size :
-            size= self.batch_size
+        if not size:
+            size = self.batch_size
 
         return self.generator(self.latent_generator(size), training=training)
 
@@ -60,16 +67,16 @@ class Simple_GAN(keras.Model) :
         """Get a real sample of size batch_size"""
 
         # Verification of size and asigning value to size
-        if not size :
-            size= self.batch_size
+        if not size:
+            size = self.batch_size
 
         sorted_indices = tf.range(tf.shape(self.real_examples)[0])
-        random_indices  = tf.random.shuffle(sorted_indices)[:size]
+        random_indices = tf.random.shuffle(sorted_indices)[:size]
 
         return tf.gather(self.real_examples, random_indices)
 
     # overloading train_step()
-    def train_step(self,useless_argument):
+    def train_step(self, useless_argument):
         """Train the GAN for one step"""
         for _ in range(self.disc_iter):
             # Real and fake samples
